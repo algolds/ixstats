6a0466ce41dfd3a1806cf5b5631b000938eba4a8
algolds <theaeguru@gmail.com>
2025-05-31 16:50:24 -0400

    country detail clean

--- src/app/countries/_components/categories/index.ts
+++
@@ -1,6 +0,0 @@
-// src/app/countries/_components/categories/index.ts
-export { PopulationCategory } from './PopulationCategory';
-export { EconomyCategory } from './EconomyCategory';
-export { GovernmentCategory } from './GovernmentCategory';
-export { SocietyCategory } from './SocietyCategory';
-export { GlobalRankCategory } from './GlobalRankCategory';
