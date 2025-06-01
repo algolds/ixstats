a311848411428976603315b71fb4b21b23668b25
algolds <theaeguru@gmail.com>
2025-06-01 18:34:49 -0400

    country page refacor

--- src/app/countries/_components/detail/index.ts
+++ src/app/countries/_components/detail/index.ts
@@ -1,10 +1,4 @@
 // src/app/countries/_components/detail/index.ts
 // Detail page component exports
 
-export { TimeControl } from './TimeControl';
 export { CountryAtGlance } from './CountryAtGlance';
-export { TenYearForecast } from './TenYearForecast';
-export { ChartTypeSelector } from './ChartTypeSelector';
-
-// Re-export types
-export type { ChartType } from './ChartTypeSelector';
