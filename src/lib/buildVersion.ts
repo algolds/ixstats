6b437a65db632aa78c6aea31cfb0ce05ce2a5c83
IxWiki Admin <admin@ixwiki.com>
2026-05-27 23:47:45 -0400

    chore: bump IXWORLD_VERSION to 2.2.1

--- src/lib/buildVersion.ts
+++ src/lib/buildVersion.ts
@@ -3,7 +3,7 @@ export { BUILD_VERSION } from "./buildVersion.generated";
 
 export const APP_VERSION = "2.0";
 export const WIKIOS_VERSION = "2.0-alpha";
-export const IXWORLD_VERSION = "2.2.0";
+export const IXWORLD_VERSION = "2.2.1";
 export const IXFORUM_VERSION = "1.0-alpha";
 export const THINKPAGES_VERSION = "2.0";
 export const BUILDER_VERSION = "2.0";
