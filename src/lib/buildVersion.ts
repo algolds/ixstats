66270f3d19c276f18e799af03a21ea8eeb28ff5c
IxWiki Admin <admin@ixwiki.com>
2026-06-07 19:00:27 -0400

    chore: update WIKIOS and THINKPAGES version numbers

--- src/lib/buildVersion.ts
+++ src/lib/buildVersion.ts
@@ -2,8 +2,8 @@
 export { BUILD_VERSION } from "./buildVersion.generated";
 
 export const APP_VERSION = "2.1";
-export const WIKIOS_VERSION = "1.1";
+export const WIKIOS_VERSION = "1.2";
 export const IXWORLD_VERSION = "2.3.0";
 export const IXFORUM_VERSION = "1.0-preview";
-export const THINKPAGES_VERSION = "1.0-preview";
+export const THINKPAGES_VERSION = "1.0";
 export const BUILDER_VERSION = "3.1-preview";
