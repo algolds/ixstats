5e5861ddedb471ec7a4f53be24aa50bd8d772df4
IxWiki Admin <admin@ixwiki.com>
2026-06-08 06:24:51 -0400

    chore: update WIKIOS_VERSION to 1.3 in buildVersion.ts

--- src/lib/buildVersion.ts
+++ src/lib/buildVersion.ts
@@ -2,7 +2,7 @@
 export { BUILD_VERSION } from "./buildVersion.generated";
 
 export const APP_VERSION = "2.1";
-export const WIKIOS_VERSION = "1.2";
+export const WIKIOS_VERSION = "1.3";
 export const IXWORLD_VERSION = "2.3.0";
 export const IXFORUM_VERSION = "1.0-preview";
 export const THINKPAGES_VERSION = "1.0";
